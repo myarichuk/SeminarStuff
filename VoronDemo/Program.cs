9cb2e3975f8f790c92343a87bd674bef492afa4e
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-03-27 20:39:30 +0700

    simplified Voron demo

--- VoronDemo/Program.cs
+++ VoronDemo/Program.cs
@@ -12,12 +12,15 @@ namespace VoronDemo
 {
     class Program
     {
-        private const int TestElementSize = 100;
+        private const int TestElementSize = 128;
         private const string TreeName = "TestTree";
-        private const int TestElementsCount = 1024 * 1024;
+        private const int TestElementsCount = 1024 * 16;
 
         static void Main(string[] args)
         {
+            //in 32bit mode it might be impossible to find continuous blocks of large enough memory
+            //if TestElementsCount or TestElementSize are big
+            Console.WriteLine("Is running under 64 bits? " + Environment.Is64BitProcess);
             Console.Write("Generating random {0} byte values...", TestElementSize);
             var testData = GenerateTestData(TestElementsCount).ToList();
             Console.WriteLine("done");
@@ -30,62 +33,38 @@ namespace VoronDemo
                     env.CreateTree(tx, TreeName);
                     tx.Commit();
                 }
-                int keyIndex = 0;
-                Console.WriteLine("Read/write test with sequential keys");
-                Console.WriteLine("----------------------------------");
-                PerformanceTest(testData, () => "testElement/" + (keyIndex++), env);
-            }
-        }
+              
+                //first write data
+                int index = 1;
+                var writeBatch = new WriteBatch();
+                foreach (var element in testData)
+                    writeBatch.Add("element/" + index++,new MemoryStream(element), TreeName);
 
-        private static void PerformanceTest(IEnumerable<byte[]> testData,Func<string> keyGeneratorFunc, StorageEnvironment env)
-        {
-//////////////////////////////////////////////////////////////////////////////////////////
-            // write
-            var writeBatch = new WriteBatch();
-            int index = 1;
-            Console.Write("Adding values to WriteBatch...");
-            foreach (var testElement in testData)
-            {
-                var elementStream = new MemoryStream(testElement);
-                writeBatch.Add(keyGeneratorFunc(), elementStream, TreeName);
-            }
-            Console.WriteLine("done");
+                var sw = Stopwatch.StartNew();
+                env.Writer.Write(writeBatch);
+                sw.Stop();
 
-            var totalWriteTimer = Stopwatch.StartNew();
-            Console.Write("Writing the data to Voron storage...");
-            env.Writer.Write(writeBatch);
-            totalWriteTimer.Stop();
-            Console.WriteLine("done");
-            Console.WriteLine("Time that took to write 1000000 elements is {0}ms", totalWriteTimer.ElapsedMilliseconds);
-            Console.WriteLine("On the average it took {0:0##.#####}ms per item",
-                (decimal) totalWriteTimer.ElapsedMilliseconds/TestElementsCount);
+                Console.WriteLine("took {0:##.#####}ms to write {1} elements",sw.ElapsedMilliseconds,TestElementsCount);
 
-            ///////////////////////////////////////////////////////////////////////
-            // single threaded read
-            using (var snapshot = env.CreateSnapshot())
-            {
-                Console.Write("Reading test elements (single thread)...");
-                var totalReadTimer = Stopwatch.StartNew();
-                var readBuffer = new byte[TestElementSize];
-                using (var iterator = snapshot.Iterate(TreeName))
+                //then read the data
+                sw = Stopwatch.StartNew();
+                using (var snapshot = env.CreateSnapshot())
                 {
-                    //if iterator is not empty
-                    if (iterator.Seek(Slice.BeforeAllKeys))
+                    using (var iterator = snapshot.Iterate(TreeName))
                     {
-                        do
+                        if (iterator.Seek(Slice.BeforeAllKeys))
                         {
-                            using (var valueStream = iterator.CreateReaderForCurrent().AsStream())
+                            var buffer = new byte[TestElementSize];
+                            do
                             {
-                                valueStream.Read(readBuffer, 0, TestElementSize);
-                            }
-                        } while (iterator.MoveNext());
+                                using (var valueStream = iterator.CreateReaderForCurrent().AsStream())
+                                    valueStream.Read(buffer, 0, TestElementSize);
+                            } while (iterator.MoveNext());
+                        }
                     }
                 }
-                totalReadTimer.Stop();
-                Console.WriteLine("done");
-                Console.WriteLine("Time that took to read 1000000 elements is {0}ms", totalReadTimer.ElapsedMilliseconds);
-                Console.WriteLine("On the average it took {0:0##.#####}ms per item",
-                    (decimal) totalReadTimer.ElapsedMilliseconds/TestElementsCount);
+                sw.Stop();
+                Console.WriteLine("took {0:##.#####}ms to read {1} elements", sw.ElapsedMilliseconds, TestElementsCount);
             }
         }
 
